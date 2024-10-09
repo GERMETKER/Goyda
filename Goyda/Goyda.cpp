bb555143c2c805483208f1c9119067ee9628b217
Top <Top@WIN-3ABE5TFH4JI>
2024-10-09 18:26:31 +0500

    Добавьте файлы проекта.

---
+++ Goyda/Goyda.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Animal
+{
+public:
+
+
+protected:
+    int experience;
+    int age;
+    bool sex;
+    void Kushat()
+    {
+
+    }
+    void Dishat()
+    {
+
+    }
+    void Reproduction()
+    {
+
+    }
+    void Ohota(std::string)
+    {
+
+    }
+    void Sleep()
+    {
+
+    }
+    void MakeSound()
+    {
+
+    }
+
+
+private:
+
+
+
+};
+class Cat : public Animal
+{
+public:
+    void CatchLaser()
+    {
+        Ohota("Laser");
+    }
+    void Rest()
+    {
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+        Kushat();
+        Sleep();
+    }
+
+private:
+    int lives = 9;
+};
+
+class Dog : public Animal
+{
+public:
+    void Wolk()
+    {
+        MakeSound();
+        MakeSound();
+        MakeSound();
+        MakeSound();
+        Ohota("Belka");
+        Ohota("Belka");
+        EatKakyyToHren();
+        EatKakyyToHren();
+        EatKakyyToHren();
+        MakeSound();
+        MakeSound();
+        MakeSound();
+
+    }
+    void StealFood()
+    {
+        Kushat();
+    }
+    void EatKakyyToHren()
+    {
+        Kushat();
+    }
+    void CatchBall()
+    {
+        Ohota("Ball");
+    }
+
+private:
+
+
+};
+
+
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+
+
+}
+
